8ea71d3b2e0562a8ed3ce8c3b0b135056e08e342
Cheese-Space <batumete1011@gmail.com>
2026-03-13 14:53:33 +0100

    add input logic

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,31 @@ mod chip8;
 use std::{env, fs, process::ExitCode};
 use crate::chip8::Chip8;
 use sdl3::event::Event;
-use sdl3::keyboard::Keycode;
+use sdl3::keyboard::{Keycode, Scancode};
 use sdl3::pixels::Color;
 use sdl3::rect::Rect;
 use std::time::Duration;
+fn set_key(machine: &mut Chip8, code: Scancode, pressed: bool) {
+    match code {
+        Scancode::_1 => machine.keys[0x1] = pressed,
+        Scancode::_2 => machine.keys[0x2] = pressed,
+        Scancode::_3 => machine.keys[0x3] = pressed,
+        Scancode::_4 => machine.keys[0xC] = pressed,
+        Scancode::Q => machine.keys[0x4] = pressed,
+        Scancode::W => machine.keys[0x5] = pressed,
+        Scancode::E => machine.keys[0x6] = pressed,
+        Scancode::R => machine.keys[0xD] = pressed,
+        Scancode::A => machine.keys[0x7] = pressed,
+        Scancode::S => machine.keys[0x8] = pressed,
+        Scancode::D => machine.keys[0x9] = pressed,
+        Scancode::F => machine.keys[0xE] = pressed,
+        Scancode::Z => machine.keys[0xA] = pressed,
+        Scancode::X => machine.keys[0x0] = pressed,
+        Scancode::C => machine.keys[0xB] = pressed,
+        Scancode::V => machine.keys[0xF] = pressed,
+        _ => ()
+    }
+}
 fn main() -> ExitCode {
     let filename = match env::args().nth(1) {
         Some(f) => f,
@@ -33,6 +54,22 @@ fn main() -> ExitCode {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();  
         'running: loop {
+            // poll for events
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                    Event::KeyDown {scancode: Some(code), ..} => {
+                        set_key(&mut machine, code, true);
+                    }
+                    Event::KeyUp {scancode: Some(code), ..} => {
+                        set_key(&mut machine, code, false);
+                    }
+                    _ => ()
+                }
+            }
             // set timers
             if machine.delay_timer == 0 {
                 machine.delay_timer = 255;
@@ -64,24 +101,8 @@ fn main() -> ExitCode {
                 canvas.present();
                 machine.refresh_display = false;
             }
-            // poll for events
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit {..} |
-                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                        break 'running
-                    },
-                    _ => {}
-                }
-            }
             // ^todo: other logic
             std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 700));
         }
     ExitCode::SUCCESS
 }
-
-
-
-
-
-
